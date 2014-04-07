01c0660142a8c4db21fc4e607438e93352f1c09d
Lilas Dinh <lvkdinh@gmail.com>
2014-04-07 17:07:21 -0400

    Testing frame starting

---
+++ test/driver.py
@@ -0,0 +1,27 @@
+"""A Testing Module that makes random multiple choice questions from a
+text file."""
+import math
+class Tests:
+    dict = {}
+    def __init__(self):
+        pass
+    def start_test(self):
+        pass
+    def getdict(self):
+        return dict
+    def run(self):
+        pass
+
+def getRand():
+    pass
+
+def makeSuite():
+    pass
+
+def getLib():
+    pass
+
+def main(): #Run driver
+    pass
+
+if __name__ == "__main__": main()
