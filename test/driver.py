db718727aa8c5e051e842e2ba6f50e30c7b9e3bb
Lilas Dinh <lvkdinh@gmail.com>
2014-04-09 12:42:39 -0400

    object devel

--- test/driver.py
+++ test/driver.py
@@ -1,44 +1,38 @@
 """A Testing Module that makes random multiple choice questions from a text file."""
 import sys
 
-class Tests:
+class tests:
     dict = {}
     x = 0
-    def __init__(self):
-        pass
-    def start_test(self):
-        pass
-    def getdict(self):
-        return dict
-    def run(self):
-        pass
+    def __init__(self, filename):
+        file = open(filename, "rU")
+        self.dict = tests.makeSuite(tests.makeTest(file.readlines()))
+        self.x = len(self.dict.items())
+    def makeSuite(text):
+        return {text[i].rstrip():[eval(text[i+1]), text[i+2].rstrip()] for i in range(0, len(text), 3)}
+    def makeTest(list):
+        i = 0
+        while i < len(list)-1:
+            if list[i+1] == '\n':
+                list.pop(i+1)
+                i += 1
+            elif list[i+1][0] == '[':
+                i += 2
+            else:
+                list[i] += list.pop(i+1)
+        return list
+    def __str__(self):
+        str = ""
+        for k, v in self.dict.items():
+            str += "Question:\n{}\n\tChoices:\n\t{}\n\tAnswer: {}\n".format(k, v[0], v[1])
+        return str
 
-def getRand():
-    pass
-
-def makeSuite(text):
-    return {text[i].rstrip():[eval(text[i+1]), text[i+2].rstrip()] for i in range(0, len(text), 3)}
-
-def makeTest(list):
-    i = 0
-    while i < len(list)-1:
-        if list[i+1] == '\n':
-            list.pop(i+1)
-            i += 1
-        elif list[i+1][0] == '[':
-            i += 2
-        else:
-            list[i] += list.pop(i+1)
-
-def askQuestion(list, answer):
+def askQuestion(list):
     print(list[0])
     choice = {num:choice for num, choice in enumerate(list[1], 1)}
 
 def main(args): #Run driver
-    filename = open("multiple_choice.txt", "rU")
-    text = filename.readlines()
-    makeTest(text)
-    dict = makeSuite(text)
-    print(dict)
+    x = tests("multiple_choice.txt")
+    print(x)
     
 if __name__ == "__main__": main(sys.argv)
