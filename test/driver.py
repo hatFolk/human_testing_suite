bc6b13389b4afe5feeb9a2392e6bcd614ba4f069
Lilas Dinh <lvkdinh@gmail.com>
2014-04-09 12:47:23 -0400

    fixing the class

--- test/driver.py
+++ test/driver.py
@@ -2,25 +2,30 @@
 import sys
 
 class tests:
-    dict = {}
-    x = 0
+    database = {}
+    numQuestions = 0
     def __init__(self, filename):
+        """Tests is initialized with a filename containing Questions. This builds the database to test from"""
         file = open(filename, "rU")
-        self.dict = tests.makeSuite(tests.makeTest(file.readlines()))
-        self.x = len(self.dict.items())
-    def makeSuite(text):
+        self.database = tests.makeSuite(tests.makeTest(file.readlines()))
+        self.numQuestions = len(self.dict.items())
+    def makeSuite(self, text):
         return {text[i].rstrip():[eval(text[i+1]), text[i+2].rstrip()] for i in range(0, len(text), 3)}
-    def makeTest(list):
+    def makeTest(self, xs):
         i = 0
-        while i < len(list)-1:
-            if list[i+1] == '\n':
-                list.pop(i+1)
+        while i < len(xs)-1:
+            if xs[i+1] == '\n':
+                xs.pop(i+1)
                 i += 1
-            elif list[i+1][0] == '[':
+            elif xs[i+1][0] == '[':
                 i += 2
             else:
-                list[i] += list.pop(i+1)
-        return list
+                xs[i] += xs.pop(i+1)
+        return xs
+    def getDatabase(self):
+        return self.database
+    def __len__(self):
+        return numQuestions
     def __str__(self):
         str = ""
         for k, v in self.dict.items():
