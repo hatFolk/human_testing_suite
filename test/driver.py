290625cbf4bcd2c5d492c0c18e44e702dfc1ec34
Lilas Dinh <lvkdinh@gmail.com>
2014-04-11 02:28:24 -0400

    Can now be imported as an object. Working on flexibility and features.

--- test/driver.py
+++ test/driver.py
@@ -1,15 +1,19 @@
 """A Testing Module that makes random multiple choice questions from a text file."""
 import sys
+import random
+
 class tests:
     database = {}
     numQuestions = 0
     def __init__(self, filename):
         """Tests is initialized with a filename containing Questions. This builds the database to test from"""
         file = open(filename, "rU")
-        self.database = tests.makeSuite(tests.makeTest(file.readlines()))
-        self.numQuestions = len(self.dict.items())
+        self.database = tests.makeSuite(self, tests.makeTest(self, file.readlines()))
+        self.numQuestions = len(self.database)
+    
     def makeSuite(self, text):
         return {text[i].rstrip():[eval(text[i+1]), text[i+2].rstrip()] for i in range(0, len(text), 3)}
+
     def makeTest(self, xs):
         i = 0
         while i < len(xs)-1:
@@ -21,22 +25,53 @@ class tests:
             else:
                 xs[i] += xs.pop(i+1)
         return xs
+
     def getDatabase(self):
         return self.database
+
+    def askQuestion(question, list):
+        print(question)
+        choice = {k:v for k, v in enumerate(list[0])}
+        for i in choice.items().sort():
+            print("\t{}. {}".format(i[0], i[1]))
+        ans = input("[Pick your number] > ")
+        try:
+            print("Correct!" if list[1] == choice[int(ans)] else "Wrong..., the answer was : {}".format(list[1]))
+            return list[1] == choice[int(ans)]
+        except (KeyError, ValueError):
+            print("Invalid answer...")
+
+    def startQuestions():
+        questions = list(self.database.keys())
+        random.shuffle(questions)
+        ctr = self.numQuestions
+        points = 0
+        while question: #i.e., questions still have things in it
+            if tests.askQuestion(questions[ctr], self.database[questions[ctr]]):
+                points += 1
+            choice = input("\t[Remove Question? Type 'Yes' or 'yes', else it will be kept] > ")
+            if choice.lower() == 'yes':
+                questions.pop()
+                ctr -= 1
+        print("That's all the questions! Your Percent Accuracy: {}".format(points/self.numQuestions))
+
     def __len__(self):
-        return numQuestions
+        return self.numQuestions
+
     def __str__(self):
         str = ""
         for k, v in self.dict.items():
             str += "Question:\n{}\n\tChoices:\n\t{}\n\tAnswer: {}\n".format(k, v[0], v[1])
         return str
 
-def askQuestion(list):
-    print(list[0])
-    choice = {num:choice for num, choice in enumerate(list[1], 1)}
+def main(filenames):
+    """If filenames are passed in, they will be created as tests and then tested one by one"""
+    if(filenames):
+        for i in filenames:
+            try:
+                test_n = tests(i)
+                test_n.startQuestions()
+            except (FileNotFoundError, IndexError) as e:
+                print("Problem! You got this : {}".format(e))
 
-def main(args): #Run driver
-    x = tests("multiple_choice.txt")
-    print(x)
-    
-if __name__ == "__main__": main(sys.argv)
+if __name__ == "__main__" : main(sys.argv)
