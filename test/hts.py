a84ba7bffba6016ba0726d31ed4ce147f64cac90
Lilas Dinh <lvkdinh@gmail.com>
2014-04-17 04:29:42 -0400

    parsing done. question class essentially done

--- test/hts.py
+++ test/hts.py
@@ -7,10 +7,9 @@ class Tests:
     numQuestions = 0
     def __init__(self, filename):
         file = open(filename, "rU")
-        qlist = file.read().split('\n\n\n')
-        qlist = Tests.makeTest(self, qlist)
-        for i in range(len(qlist)):
-            print("{} : {}".format(i, qlist[i]))
+        qlist = file.read()
+        qlist = self.makeTest(qlist)
+        self.makeSuite(qlist)
         file.close()
         
     def makeSuite(self, text):
@@ -19,19 +18,25 @@ class Tests:
         Precondition: That the list is properly formatted."""
         i = 0
         while i < len(text):
-            self.database[self.numQuestions] = Question(text[i].rstrip(), text[i+1])
+            self.database[self.numQuestions] = Question(text[i][0], text[i+1])
             self.numQuestions += 1 
             i += 2
-        
-    def makeTest(self, text): # Fix the logic. Make simpler?
-        i =0 
-        while i < len(text):
-            if text[i] == '':
-                del text[i]
-            if type(text[i]) == type([]):
-                print('Hi')
-            return
             
+    def makeTest(self, text):
+        text = text.split('\n\n\n')
+        text = [i.split('***') for i in text if i[0]]
+        i = 0
+        while i < len(text):
+            j = 0
+            while j < len(text[i]): # You could probably do this with a combination of filter and map...
+                if text[i][j] == '':
+                    del text[i][j]
+                else:
+                    text[i][j] = text[i][j].rstrip()
+                    j+=1
+            i += 1
+        return text
+    
     def getDatabase(self):
         return self.database
 
@@ -47,20 +52,6 @@ class Tests:
         except (KeyError, ValueError): # Exception handling...
                 print("Invalid answer...")
 
-    def startQuestions(self, x = numQuestions):
-        points = 0
-        ctr = 0
-        questions = list(self.database.keys())[:x]
-        random.shuffle(questions)
-        print(questions)
-        while questions: #i.e., questions still have things in it
-            if tests.askQuestion(questions[ctr], self.database[questions[ctr]]):
-                points += 1
-            choice = input("\t[Remove Question? Type 'Yes' or 'yes', else it will be kept] > ")
-            if choice.lower() == 'yes':
-                questions.pop(ctr)        
-        print("points = {}; x = {} ;".format(points, x)) 
-        print("That's all the questions! Your Percent Accuracy: {}".format(points/x))
     def __iter__(self):
         x = list(range(self.numQuestions))
         for i in random.shuffle(x):
@@ -78,16 +69,21 @@ class Tests:
 class Question:
     """Question object assumes that the first multiple choice answer is the correct answer """
     question = ""
-    choice = []
+    choice = {}
     ans = ""
     def __init__(self, question, choice): # Assumes the answer is the first value of choice
+        random.seed()
         self.question = question
-        self.choice = choice
         self.ans = choice[0]
+        self.choice = self.getChoice(choice)
+        print(self.choice)
     def getQuestion(self):
         return question
-    def getChoice(self): # Randomizes choices and assigns a numerical for each.
-        return {k+1:v for k, v in enumerate(random.shuffle(choice))}
+    def getChoice(self, mult): # Randomizes choices and assigns a numerical for each.
+        print(mult)
+        random.shuffle(mult)
+        return {k[0]:k[1] for k in enumerate(mult, 1)}
+    
     def getAnswer(self):
         return ans
     def askQuestion(self):
@@ -96,7 +92,13 @@ class Question:
         for i in list(multChoice.keys()).sort():
             print("{}.\n{}".format(i, multChoice[i]))
     def __str__(self):
-        return "Question:\n\t{}\nChoices:\n\t{}\nAnswer\n\t{}".format(self.question, self.choice, self.ans)
+        quest = ""
+        quest += "Question : {}\n".format(self.question)
+        for i in sorted(self.choice):
+            if self.choice[i] == self.ans:
+                quest += "***"
+            quest += "{:0}\t:{:1}\n".format(i)
+        return quest
 
 def storeToFile(filename, test):
     file = open(test, "w+")
@@ -104,15 +106,16 @@ def storeToFile(filename, test):
     for i in data.values():
         return
 
+def startTest(test):
+
 def main(filenames):
     """If filenames are passed in, they will be created as tests and then tested one by one"""
     if(filenames):
         for i in filenames:
             try:
                 newTest = Tests(i)
-                print(newTest)
                 #newTest.startQuestions()
             except (FileNotFoundError, IndexError) as e:
-                raise Exception("Ran into a problem! : {}".format(e))
+                
 
 if __name__ == "__main__" : main(sys.argv[1:]) # element 0 only interesting if you want the name of your file...
