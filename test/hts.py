f299f819dc958fa6dfdf0706a8101558175a7cdd
Lilas Dinh <lvkdinh@gmail.com>
2014-04-17 20:32:39 -0400

    Polished prints

--- test/hts.py
+++ test/hts.py
@@ -3,26 +3,23 @@ import sys
 import random
 # Such OOP. Very wow.
 class Test:
-    """Takes w """
+    """Takes a file, breaks it up into Question and
+    Multiple Choices and make Question objects"""
     database = {}
     numQuestions = 0
     def __init__(self, filename):
+        """Constructor for Tests.
+        Takes a file and parses it for Questions"""
         file = open(filename, "rU")
         qlist = file.read()
         qlist = self.makeTest(qlist)
         self.makeSuite(qlist)
         file.close()
         
-    def makeSuite(self, text):
-        i = 0
-        while i < len(text):
-            self.database[self.numQuestions] = Question(text[i][0], text[i+1])
-            self.numQuestions+=1
-            i+=2
-
     def makeTest(self, text):
+        """Take a block of text from a file and makes it usable for makeSuite."""
         text = text.split('\n\n\n')
-        text = [i.split('***') for i in text if i[0]]
+        text = [i.split('***') for i in text]
         i = 0
         while i < len(text):
             j = 0
@@ -34,20 +31,31 @@ class Test:
                     j+=1
             i += 1
         return text
-    
-    def getDatabase(self):
-        return self.database
 
+    def makeSuite(self, text):
+        """Takes a parsed text list and puts it into a Question object database.
+        Precondition: text has already been through makeTest"""
+        i = 0
+        while i < len(text):
+            self.database[self.numQuestions] = Question(text[i][0], text[i+1])
+            self.numQuestions+=1
+            i+=2
+    
     def __iter__(self):
-        x = list(range(self.numQuestions))
+        """Makes Test iterable after shuffling the questions"""
+        x = list(range(1, self.numQuestions))
         random.shuffle(x)
         for i in x:
             yield self.database[i]
-            
+        raise StopIteration
+
     def __len__(self):
+        """The length of Tests should be the number of Questions...
+        Which is numQuestions."""
         return self.numQuestions
 
     def __str__(self):
+        """And if you print a Test, what does it look like?"""
         str = ""
         for k, v in self.database.items():
             str += "Question {}:\n{}".format(k, v)
@@ -59,6 +67,7 @@ class Question:
     choice = {}
     ans = ""
     def __init__(self, question, choice): # Assumes the answer is the first value of choice
+        """Constructor for Questions."""
         random.seed()
         self.question = question
         self.ans = choice[0]
@@ -74,16 +83,18 @@ class Question:
         """Asks the question. Gets an answer.
         Returns False if answered correctly. Returns True."""
         print(self.question)
-        multChoice = getChoice(self)
-        for i in sorted(multChoice):
-            print("{}.\n{}".format(i, multChoice[i]))
+        for i in sorted(self.choice):
+            print("{}.\n    {}".format(i, self.choice[i].replace('\n','\n    ')))
+        print("")
         num = input("[Which number is the answer?] > ")
+        print("")
         try:
             return not (self.choice[int(num)] == self.ans)
         except:
             return True
             
     def __str__(self):
+        """Mainly for Debug and telling people that they did something wrong."""
         quest = ""
         quest += "{}\n\n".format(self.question)
         for i in sorted(self.choice):
@@ -101,8 +112,10 @@ def main(filenames):
             for j in newTest:
                 if j.askQuestion():
                     answeredWrong.append(j)
-            print("Please review these questions!")
+            print("Please review these questions!:\n")
+            i = 1
             for k in answeredWrong:
-                print(k.question)
+                print("{}.\n    {}\n".format(i, k.question.replace('\n', '\n    ')))
+                i += 1
 
 if __name__ == "__main__" : main(sys.argv[1:]) # element 0 only interesting if you want the name of your file...
